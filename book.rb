b7eae75b267bf221be6aea2d4ce0d69b953a7ebe
sheezu900 <shahzamanlive2@gmail.com>
2023-10-07 16:17:53 +0500

    create relations

--- book.rb
+++ book.rb
@@ -1,9 +1,17 @@
+require_relative 'rental'
+
 # cleate book class
 class Book
   def initialize(title, auther)
     @title = title
     @auther = auther
+    @rental = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(date, self, person)
   end
 
   attr_accessor :title, auther
+  attr_reader :rental
 end
