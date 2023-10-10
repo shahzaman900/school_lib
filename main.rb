b3c12524aaf25bc08f4d0e633f08c0244e0cfbce
sheezu900 <shahzamanlive2@gmail.com>
2023-10-10 10:34:57 +0500

    complete refector code

--- main.rb
+++ main.rb
@@ -1,50 +1,17 @@
 require_relative 'app'
 require_relative 'options'
 
-def all_options
-  puts "\nPlease enter a number:\n
-1 - List all Books\n
-2 - List all People\n
-3 - Create a Person\n
-4 - Create a Book\n
-5 - Create a Rental\n
-6 - List all rentals for a given person id\n
-7 - Exit"
-end
-
 def prompt
   puts 'Welcome to School Lib!'
+  list = Options.new
+
   loop do
-    all_options
+    list.all_options
     option = gets.chomp.to_i
     break if option == 7
 
-    call_option(option)
+    list.call_option(option)
   end
 end
 
-def call_option(option)
-  case option
-  when 1
-    list_books
-  when 2
-    list_people
-  when 3
-    create_person
-  when 4
-    create_book
-  when 5
-    create_rental
-  when 6
-    list_rentals
-  else
-    puts 'Choose between 1 - 7'
-  end
-end
-
-def main
-  app = App.new
-  app.run
-end
-
-main
+prompt
